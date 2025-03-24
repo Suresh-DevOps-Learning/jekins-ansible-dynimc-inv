ff1c1e2125c698ebbab5153a0c4d6dd418f1c35d
Uppu-Suresh <uppusuresh1999@gmail.com>
2025-03-24 11:37:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
-provider "aws"{
-region = "ap-south-1"
+provider "aws" { 
+region="ap-south-1" 
 }
-
-resource "aws_instance" "AWSEC2Instance"{
-   ami = "ami-0d682f26195e9ec0f"
-   instance_type = "t2.micro"
-   key_name = "devopsjan2025"
-   security_groups = ["launch-wizard-1"]
-   tags={
-   Name = "terraformserver"
-   }
+ 
+resource "aws_instance" "AWSEC2Instance"{ 
+ami = "ami-05c179eced2eb9b5b" 
+instance_type = "t2.micro" 
+key_name = "testpemfile" 
+security_groups = ["launch-wizard-2"] 
+tags={ 
+Name = "terraformserver" 
+} 
 }
